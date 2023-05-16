ed9ba6ad708292b4867ffe6fbd0b2e177072f364
Martin Dimitrov <martin.p.dimitrov@intel.com>
2023-05-16 14:03:57 -0700

    added --branch to redis installation

--- run-benchmark-spec.sh
+++ run-benchmark-spec.sh
@@ -77,7 +77,7 @@ if ! $SSH_COMMAND command -v "$REDIS_PATH/src/redis-server" &>/dev/null; then
 	$SSH_COMMAND apt install make -y
 	$SSH_COMMAND apt install gcc -y
 	$SSH_COMMAND apt install pkg-config -y
-	$SSH_COMMAND git clone --recursive https://github.com/redis/redis.git
+	$SSH_COMMAND git clone --recursive https://github.com/redis/redis.git --branch $REDIS_BRANCH
 	$SSH_COMMAND "cd $REDIS_PATH; make -j"
 	$SSH_COMMAND "cd $REDIS_PATH; cat redis.conf | sed \"s/bind/#bind/\" > redis.conf.new"
 	$SSH_COMMAND "cd $REDIS_PATH; cat redis.conf.new | sed \"s/protected-mode yes/protected-mode no/\" > redis.conf.new2"
