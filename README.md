49b4781772ad3e558ba0081260155b0d10b8970e
Martin Dimitrov <martin.p.dimitrov@intel.com>
2023-08-16 11:26:23 -0700

    removed files from old location

--- README.md
+++
@@ -1,4 +0,0 @@
-# redis-scripts
-
-Repository of helper scripts indended to faciliate Redis benchmarking and telemetry collection. 
-
--- postscripts/combine_perf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grep -E '\[*\]' -H *-perf.txt | sort -g -r -k 2 > all.combined.report
-grep -E '\[k\]'  all.combined.report > kernel.combined.report
-grep -E '\[\.\]' all.combined.report > user.combined.report
