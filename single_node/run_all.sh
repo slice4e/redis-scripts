46f11c104c4ab972a1678a1244166042b81e5184
Martin Dimitrov <martin.p.dimitrov@intel.com>
2023-08-31 12:10:22 -0700

    pulled install pre-requisites into a commmon script. Added number of iterations to single-node script

--- single_node/run_all.sh
+++ single_node/run_all.sh
@@ -27,6 +27,10 @@ fi
 
 echo "SSH Connection is Successfull!"
 
+#---------------------------------------------------------- Install Pre-reqs -------------------------------------------------------
+source $PREREQS_SCRIPT
+
+
 mkdir -p ${RESULTS_PATH}
 cp $config_file ${RESULTS_PATH}
 
@@ -72,104 +76,117 @@ if [[ $SET_IRQ == true ]]; then
 	source $SET_IRQ_PATH $CPUS
 fi
 
-
-#--------------------------start master servers------------------------------------------------------
-instances=1
-for cpu in $CPUS
+for (( iteration=1; iteration <= $ITERATION_NUM; iteration++ ))
 do
-	port=$(($START_PORT + ${instances}))
-	echo -e "starting redis server $instances on vCPU $cpu"
-	cmd="numactl -m ${SERVER_SOCKET} taskset -c $cpu  $REDIS_PATH/src/redis-server $REDIS_PATH/redis.conf --port ${port} --save \"\" "
-    	echo -e $cmd
-	$cmd & 
-	instances=$((instances + 1))
 
-	if [ $instances -gt $NUM_SERVERS ]
-	then
-		break
+	mkdir ${RESULTS_PATH}/run${iteration}
+
+	#--------------------------start master servers------------------------------------------------------
+	instances=1
+	for cpu in $CPUS
+	do
+		port=$(($START_PORT + ${instances}))
+		echo -e "starting redis server $instances on vCPU $cpu"
+		cmd="numactl -m ${SERVER_SOCKET} taskset -c $cpu  $REDIS_PATH/src/redis-server $REDIS_PATH/redis.conf --port ${port} --save \"\" "
+		echo -e $cmd
+		$cmd & 
+		instances=$((instances + 1))
+
+		if [ $instances -gt $NUM_SERVERS ]
+		then
+			break
+		fi
+	done
+
+	while [ $(ps -e | grep -c redis-server) -lt $NUM_SERVERS ];do
+		echo -e "Waiting for all redis servers to start"
+		sleep 5
+	done
+
+	echo "$(($(ps -e | grep -c redis-server))) redis servers started"
+
+
+	#--------------------------start memtier benchmark FILL ---------------------------------------------
+	instances=1
+	for cpu in $MEMTIER_CPUS
+	do
+		port=$(($START_PORT + ${instances}))
+		echo -e "starting memtier benchmark $instances on vCPU $cpu"
+		cmd="numactl -m ${MEMTIER_SOCKET} taskset -c $cpu ${MEMTIER_PATH}/memtier_benchmark -s $SERVER_IP -p ${port} --hide-histogram --key-maximum=${NUM_FILL_REQ} -n allkeys --data-size-list=${DATA_SIZE_LIST} --pipeline=15 --key-pattern=P:P --ratio=1:0 --out-file=${RESULTS_PATH}/run${iteration}/fill_$instances.log"
+		instances=$((instances + 1))
+		echo -e $cmd
+		$cmd >/dev/null &
+		
+		if [ $instances -gt $NUM_SERVERS ]
+		then
+			break
+		fi
+	done
+
+	while [ $(ps -ef | grep -c memtier_benchmark) -gt 1 ];do
+		echo -e "Waiting for $(($(ps -ef | grep -c memtier_benchmark)-1)) memtier_benchmark to finish"
+		sleep 5
+	done
+
+
+	#--------------------------start memtier benchmark BENCHMARK ------------------------------------------
+	instances=1
+	for cpu in $MEMTIER_CPUS
+	do
+		port=$(($START_PORT + ${instances}))
+		echo -e "starting memtier benchmark $instances on vCPU $cpu"
+		cmd="numactl -m ${MEMTIER_SOCKET} taskset -c $cpu ${MEMTIER_PATH}/memtier_benchmark -s $SERVER_IP -p ${port} --hide-histogram --key-maximum=${NUM_FILL_REQ} --data-size-list=${DATA_SIZE_LIST} --randomize --distinct-client-seed --key-pattern=$KEY_PATTERN --test-time=$BENCHMARK_DURATION --ratio=4:1 --pipeline=$MEMTIER_PIPELINE -c $MEMTIER_CLIENTS -t $MEMTIER_THREADS --out-file=${RESULTS_PATH}/run${iteration}/benchmark_$instances.log"
+		instances=$((instances + 1))
+		echo -e $cmd
+		$cmd >/dev/null &
+
+		if [ $instances -gt $NUM_SERVERS ]
+		then
+			break
+		fi
+	done
+
+	if [ $iteration == 1 ] && [ $RUN_EMON == true ]; then 
+		echo "Starting emon... (First, try to stop if emon is running)"
+		cmd="${EMON_FOLDER}/emon -stop "
+		$cmd
+		cmd="${EMON_FOLDER}/emon -collect-edp -f ${RESULTS_PATH}/single_node-emon.dat "
+		$cmd &
 	fi
-done
-
-while [ $(ps -e | grep -c redis-server) -lt $NUM_SERVERS ];do
-	echo -e "Waiting for all redis servers to start"
-        sleep 5
-done
-
-echo "$(($(ps -e | grep -c redis-server))) redis servers started"
 
 
-#--------------------------start memtier benchmark FILL ---------------------------------------------
-instances=1
-for cpu in $MEMTIER_CPUS
-do
-	port=$(($START_PORT + ${instances}))
-	echo -e "starting memtier benchmark $instances on vCPU $cpu"
-	cmd="numactl -m ${MEMTIER_SOCKET} taskset -c $cpu ${MEMTIER_PATH}/memtier_benchmark -s $SERVER_IP -p ${port} --hide-histogram --key-maximum=${NUM_FILL_REQ} -n allkeys --data-size-list=${DATA_SIZE_LIST} --pipeline=15 --key-pattern=P:P --ratio=1:0 --out-file=${RESULTS_PATH}/fill_$instances.log"
-	instances=$((instances + 1))
-    	echo -e $cmd
-	$cmd >/dev/null &
-	
-	if [ $instances -gt $NUM_SERVERS ]
-	then
-		break
+	while [ $(ps -ef | grep -c memtier_benchmark) -gt 1 ];do
+		echo -e "Waiting for $(($(ps -ef | grep -c memtier_benchmark)-1)) memtier_benchmark to finish"
+		sleep 5
+	done
+
+	echo "Killing existing redis server instances and remove rdb files..."
+	killall -9 redis-server
+	while [ $(ps -ef | grep -c redis-server) -gt 1 ];do
+		echo -e "Waiting for $(($(ps -ef | grep -c redis-server)-1)) to die"
+		sleep 1
+	done
+	rm -f ${RDB_PATH}/*.rdb
+
+
+	if [ $iteration == 1 ] && [ $RUN_EMON == true ]; then 
+		cmd="${EMON_FOLDER}/emon -stop "
+		$cmd 
 	fi
+
+	#-------------------------- Process Results ------------------ ------------------------------------------
+
+	echo "Total Ops/sec"
+	total_ops=`cat ${RESULTS_PATH}/run${iteration}/benchmark_* | grep Totals | awk -F " " '{total += $2; count++ } END { print total} '`
+	echo $total_ops
+	echo "Num_servers_$NUM_SERVERS,Total.Ops/sec,$total_ops" > ${RESULTS_PATH}/single_node-run${iteration}.csv 
+
+
 done
-
-while [ $(ps -ef | grep -c memtier_benchmark) -gt 1 ];do
-	echo -e "Waiting for $(($(ps -ef | grep -c memtier_benchmark)-1)) memtier_benchmark to finish"
-        sleep 5
-done
-
-
-#--------------------------start memtier benchmark BENCHMARK ------------------------------------------
-instances=1
-for cpu in $MEMTIER_CPUS
-do
-	port=$(($START_PORT + ${instances}))
-	echo -e "starting memtier benchmark $instances on vCPU $cpu"
-	cmd="numactl -m ${MEMTIER_SOCKET} taskset -c $cpu ${MEMTIER_PATH}/memtier_benchmark -s $SERVER_IP -p ${port} --hide-histogram --key-maximum=${NUM_FILL_REQ} --data-size-list=${DATA_SIZE_LIST} --randomize --distinct-client-seed --key-pattern=$KEY_PATTERN --test-time=$BENCHMARK_DURATION --ratio=4:1 --pipeline=$MEMTIER_PIPELINE -c $MEMTIER_CLIENTS -t $MEMTIER_THREADS --out-file=${RESULTS_PATH}/benchmark_$instances.log"
-	instances=$((instances + 1))
-    	echo -e $cmd
-	$cmd >/dev/null &
-
-	if [ $instances -gt $NUM_SERVERS ]
-	then
-		break
-	fi
-done
-
-if [[ $RUN_EMON == true ]] ; then
-        echo "Starting emon... (First, try to stop if emon is running)"
-        cmd="${EMON_FOLDER}/emon -stop "
-        $cmd
-	cmd="${EMON_FOLDER}/emon -collect-edp -f ${RESULTS_PATH}/single_node-emon.dat "
-	$cmd &
-fi
-
-
-while [ $(ps -ef | grep -c memtier_benchmark) -gt 1 ];do
-	echo -e "Waiting for $(($(ps -ef | grep -c memtier_benchmark)-1)) memtier_benchmark to finish"
-        sleep 5
-done
-
-echo "Killing existing redis server instances and remove rdb files..."
-killall -9 redis-server
-rm -f ${RDB_PATH}/*.rdb
-
-
-if [[ $RUN_EMON == true ]] ; then
-	cmd="${EMON_FOLDER}/emon -stop "
-	$cmd 
-fi
-
-#-------------------------- Process Results ------------------ ------------------------------------------
-
-echo "Total Ops/sec"
-total_ops=`cat ${RESULTS_PATH}/benchmark_* | grep Totals | awk -F " " '{total += $2; count++ } END { print total} '`
-echo $total_ops
-echo "Num_servers_$NUM_SERVERS,Total.Ops/sec,$total_ops" > ${RESULTS_PATH}/single_node-total_ops.csv 
 
 #-------------------------- Process Emon ------------------ ------------------------------------------
+
+echo "Post processing results..."
 
 if [[ $RUN_EMON == true ]] ; then
 
@@ -182,7 +199,11 @@ if [[ $RUN_EMON == true ]] ; then
 	echo "Done post processing EMON..."
 fi
 
+CUR_DIR=`pwd`
+cd ${RESULTS_PATH}
+source $POST_SCRIPT
+cd $CUR_DIR
 
-#done
+
 
 
