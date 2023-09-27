37c5497a7216e8738a859fd1ebf16a0bf93148ba
Marcin Poźniak <marcin.pozniak@intel.com>
2023-09-27 08:50:26 +0200

    Add script to run img_query

---
+++ vecsim_research/run_img_query.py
@@ -0,0 +1,51 @@
+import numpy as np
+import argparse
+from time import time
+from redis.commands.search.query import Query
+from redis import Redis
+
+def parse_cmd_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--redis-port", "-p", dest="port", type=str, default="5000", help="Port of redis server")
+    parser.add_argument("--query-file", type=str, required=True, help="Path to file with image query embedding to run vecsim search on")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_cmd_args()
+
+    IMG_ITEM_EMBEDDING_FIELD = 'img_item_emb_vector'
+
+    host = 'localhost'
+    port = args.port
+    redis_conn = Redis(host=host, port=port)
+    print(redis_conn.ping())
+    print("Connected to Redis")
+
+    topK=5
+
+    #vectorize the query
+    query_data = np.load(args.query_file)
+    
+    for vector in query_data:
+        query_vector = vector.astype(np.float32).tobytes()
+
+        #prepare the query
+        q = Query(f'*=>[KNN {topK} @{IMG_ITEM_EMBEDDING_FIELD} $vec_param AS vector_score]').sort_by('vector_score').paging(0,topK).return_fields('url','caption','primary_key').dialect(2)
+        params_dict = {"vec_param": query_vector}
+
+        search_start_time = time()
+        #Execute the query
+        results = redis_conn.ft().search(q, query_params = params_dict)
+        search_end_time = time()
+        search_time = search_end_time - search_start_time
+        #Print similar products found
+        for product in results.docs:
+            print ('***************Item found************')
+            print ('url = ' + product.url)
+            print ('caption = ' + product.caption)
+            print ('primary_key = ' + product.primary_key)
+        print(f"Searching time: {search_time}")
+   
+if __name__ == "__main__":
+    main()
