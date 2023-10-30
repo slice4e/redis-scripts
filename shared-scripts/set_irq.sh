067c3d49da64ca3519b204e702d91f6666993477
Martin Dimitrov <martin.p.dimitrov@example.com>
2023-10-30 10:16:17 -0700

    remove the need to SSH if on local server

--- shared-scripts/set_irq.sh
+++ shared-scripts/set_irq.sh
@@ -2,26 +2,46 @@
 
 #Expects one or more cpus to which to assign the IRQs
 
-if [ "$1" != "" ]; then
+if [[ ${SERVER_REMOTE} == true ]] ; then
+	if [ "$1" != "" ]; then
 
-	echo "Assigning IRQ interruptions to CPUs $@ ...."
-	interrupts=$($SSH_COMMAND "cat /proc/interrupts | grep $IRQ_INTERFACE | awk -F ':' '{print \$1}'" | tr -d '\r')
-	for i in $interrupts
-	do
-		cmd="echo $@ > /proc/irq/${i:0:-1}/smp_affinity_list"
-		$SSH_COMMAND $cmd
-	done
+		echo "Assigning IRQ interruptions to CPUs $@ ...."
+		interrupts=$($SSH_COMMAND "cat /proc/interrupts | grep $IRQ_INTERFACE | awk -F ':' '{print \$1}'" | tr -d '\r')
+		for i in $interrupts
+		do
+			cmd="echo $@ > /proc/irq/${i}/smp_affinity_list"
+			$($SSH_COMMAND $cmd)
+		done
 
-	echo "Printing assigned cpu numbers for each interrupts..."
-	interrupts=$($SSH_COMMAND "cat /proc/interrupts | grep $IRQ_INTERFACE | awk -F ':' '{print \$1}'" | tr -d '\r' )
-	for i in $interrupts
-	do
-		cmd="cat /proc/irq/${i:0:-1}/smp_affinity_list"
-		$SSH_COMMAND $cmd
-	done
+		echo "Printing assigned cpu numbers for each interrupts..."
+		interrupts=$($SSH_COMMAND "cat /proc/interrupts | grep $IRQ_INTERFACE | awk -F ':' '{print \$1}'" | tr -d '\r' )
+		for i in $interrupts
+		do
+			cmd="cat /proc/irq/${i}/smp_affinity_list"
+			$($SSH_COMMAND $cmd)
+		done
 
+	else
+		echo "Please pass at least one argument for CPU: set_irq.sh 0" 
+	fi
 else
-	echo "Please pass at least one argument for CPU: set_irq.sh 0" 
+	if [ "$1" != "" ]; then
+
+		echo "Assigning IRQ interruptions to CPUs $@ ...."
+		interrupts=$(cat /proc/interrupts | grep $IRQ_INTERFACE | awk -F ':' '{print $1}')
+		for i in $interrupts
+		do
+			echo $@ > /proc/irq/${i}/smp_affinity_list
+		done
+
+		echo "Printing assigned cpu numbers for each interrupts..."
+		interrupts=$(cat /proc/interrupts | grep $IRQ_INTERFACE | awk -F ':' '{print $1}')
+		for i in $interrupts
+		do
+			cat /proc/irq/${i}/smp_affinity_list
+		done
+
+	else
+		echo "Please pass at least one argument for CPU: set_irq.sh 0" 
+	fi
 fi
-
-
--- shared-scripts/set_ssh.sh
+++ shared-scripts/set_ssh.sh
@@ -3,38 +3,45 @@
 SSH_CONNECTED=false
 SSH_COMMAND="ssh -t -o PreferredAuthentications=publickey -i ${SSH_KEY_PATH}/${SSH_KEY_NAME} -q ${LOGIN_ID}@${SERVER_IP}"
 
-echo "Logging in as \"${LOGIN_ID}\" to $SERVER_IP"
-$SSH_COMMAND 'exit'
-
-if [ "$?" -ne 0 ] ; then
-        SSH_CONNECTED=false
-
-        read -p "Couldn't connect to server. Do you want to create an ssh-key for the paswordless login? [Y]: " GenerateKey
-        GenerateKey=${GenerateKey:-Y}
-
-        case $GenerateKey in
-            y|Y)
-                echo "Genarating the key..."
-                ssh-keygen -t rsa -b 4096 -f ${SSH_KEY_PATH}/${SSH_KEY_NAME} -N "" -q -C ${LOGIN_ID}@${SERVER_IP}
-                echo "Copying key to the server..."
-                ssh-copy-id -i ${SSH_KEY_PATH}/${SSH_KEY_NAME} ${LOGIN_ID}@${SERVER_IP}
-
-                echo "Logging in as \"${LOGIN_ID}\" to $SERVER_IP"
-                $SSH_COMMAND 'exit'
-                if [ "$?" -ne 0 ] ; then
-                    SSH_CONNECTED=false
-			echo "Is root SSH login disabled? We can enable by modifying: /etc/ssh/sshd_config. " 
-			echo "Quiting..."
-                else
-                    SSH_CONNECTED=true
-                fi
-            ;;
-            *)
-                echo "Quiting..."
-                exit 1
-            ;;
-        esac
-else
-        SSH_CONNECTED=true
+if [[ ${SERVER_REMOTE} == false ]] ; then
+	SSH_COMMAND=""
+	SSH_CONNECTED=true
 fi
 
+
+if [[ ${SERVER_REMOTE} == true ]] ; then
+	echo "Logging in as \"${LOGIN_ID}\" to $SERVER_IP"
+	$SSH_COMMAND 'exit'
+
+	if [ "$?" -ne 0 ] ; then
+		SSH_CONNECTED=false
+
+		read -p "Couldn't connect to server. Do you want to create an ssh-key for the paswordless login? [Y]: " GenerateKey
+		GenerateKey=${GenerateKey:-Y}
+
+		case $GenerateKey in
+		    y|Y)
+			echo "Genarating the key..."
+			ssh-keygen -t rsa -b 4096 -f ${SSH_KEY_PATH}/${SSH_KEY_NAME} -N "" -q -C ${LOGIN_ID}@${SERVER_IP}
+			echo "Copying key to the server..."
+			ssh-copy-id -i ${SSH_KEY_PATH}/${SSH_KEY_NAME} ${LOGIN_ID}@${SERVER_IP}
+
+			echo "Logging in as \"${LOGIN_ID}\" to $SERVER_IP"
+			$SSH_COMMAND 'exit'
+			if [ "$?" -ne 0 ] ; then
+			    SSH_CONNECTED=false
+				echo "Is root SSH login disabled? We can enable by modifying: /etc/ssh/sshd_config. " 
+				echo "Quiting..."
+			else
+			    SSH_CONNECTED=true
+			fi
+		    ;;
+		    *)
+			echo "Quiting..."
+			exit 1
+		    ;;
+		esac
+	else
+		SSH_CONNECTED=true
+	fi
+fi
