4d39ef9a76a97a3adeb7c0126769a3513c1df695
Marcin Poźniak <marcin.pozniak@intel.com>
2024-02-08 12:19:14 +0100

    Add option to skip uploading data to redis instances and run benchmark on existing data

--- vecsim/vecsim-benchmark/run-vector-db-benchmark.sh
+++ vecsim/vecsim-benchmark/run-vector-db-benchmark.sh
@@ -57,78 +57,81 @@ else
 fi
 
 #----------------------------------- Check if Redis and RediSearch module exists if not prepare them ------------------------------------------------
-
-if [[ ! -d "$REDIS_PATH" ]]; then
-    echo "Redis not found in $REDIS_PATH, downloading it ..."
-    git clone https://github.com/redis/redis $REDIS_PATH
-    cd $REDIS_PATH
-    git checkout $REDIS_BRANCH
-    make
-    cd -
-fi
-
-if [ "$REDIS_CLUSTER" -eq 1 ]; then
-    REDISEARCH_LIB=$REDISEARCH_PATH/bin/linux-x64-release/coord-oss/module-oss.so
+if [ "$SKIP_UPLOAD" -eq 1 ]; then
+    ADDITIONAL_FLAGS="--skip-upload"
 else
-    REDISEARCH_LIB=$REDISEARCH_PATH/bin/linux-x64-release/search/redisearch.so
-fi
-
-
-if [[ ! -e $REDISEARCH_LIB ]]; then
-    echo "Rediseach library not found in $REDISEARCH_LIB"
-
-    if [[ ! -d "$REDISEARCH_PATH" ]]; then
-        echo "Rediseach not found in $REDIS_PATH"
-        git clone https://github.com/RediSearch/RediSearch $REDISEARCH_PATH
-        cd $REDISEARCH_PATH
-        git checkout $REDISEARCH_BRANCH
-        git submodule update --init --recursive
+    ADDITIONAL_FLAGS=""
+    if [[ ! -d "$REDIS_PATH" ]]; then
+        echo "Redis not found in $REDIS_PATH, downloading it ..."
+        git clone https://github.com/redis/redis $REDIS_PATH
+        cd $REDIS_PATH
+        git checkout $REDIS_BRANCH
+        make
+        cd -
     fi
 
     if [ "$REDIS_CLUSTER" -eq 1 ]; then
-        cd $REDISEARCH_PATH
-        $REDISEARCH_PATH/sbin/setup bash -l
-        make build COORD=oss MT=1
+        REDISEARCH_LIB=$REDISEARCH_PATH/bin/linux-x64-release/coord-oss/module-oss.so
     else
-        cd $REDISEARCH_PATH
-        $REDISEARCH_PATH/sbin/setup bash -l
-        make build
+        REDISEARCH_LIB=$REDISEARCH_PATH/bin/linux-x64-release/search/redisearch.so
+    fi
+
+
+    if [[ ! -e $REDISEARCH_LIB ]]; then
+        echo "Rediseach library not found in $REDISEARCH_LIB"
+
+        if [[ ! -d "$REDISEARCH_PATH" ]]; then
+            echo "Rediseach not found in $REDIS_PATH"
+            git clone https://github.com/RediSearch/RediSearch $REDISEARCH_PATH
+            cd $REDISEARCH_PATH
+            git checkout $REDISEARCH_BRANCH
+            git submodule update --init --recursive
+        fi
+
+        if [ "$REDIS_CLUSTER" -eq 1 ]; then
+            cd $REDISEARCH_PATH
+            $REDISEARCH_PATH/sbin/setup bash -l
+            make build COORD=oss MT=1
+        else
+            cd $REDISEARCH_PATH
+            $REDISEARCH_PATH/sbin/setup bash -l
+            make build
+        fi
+    fi
+
+    #---------------------------------------------- Run Redis Instance with Redisearch module ----------------------------------------------------------
+
+    echo "Killing existing redis server instances and remove rdb files..."
+    killall -9 redis-server
+
+    if [ "$REDIS_CLUSTER" -eq 1 ]; then
+        cd $REDIS_PATH/utils/create-cluster
+        REDISCLUSTER_SCRIPT=$REDIS_PATH/utils/create-cluster/create-cluster
+        $REDISCLUSTER_SCRIPT stop
+        $REDISCLUSTER_SCRIPT clean
+        cd -
+    else
+        rm -f ${REDIS_PATH}/*.rdb
+    fi
+
+    sleep 5
+
+    if [ "$REDIS_CLUSTER" -eq 1 ]; then
+        cd $REDIS_PATH/utils/create-cluster
+        REDISCLUSTER_CONFIG=$REDIS_PATH/utils/create-cluster/config.sh
+        echo "PORT=$((PORT-1))" > $REDISCLUSTER_CONFIG
+        echo "NODES=$CLUSTER_NODES" >> $REDISCLUSTER_CONFIG
+        echo "REPLICAS=$CLUSTER_REPLICAS" >> $REDISCLUSTER_CONFIG
+        echo "ADDITIONAL_OPTIONS='--loadmodule $REDISEARCH_LIB'" >> $REDISCLUSTER_CONFIG
+        $REDISCLUSTER_SCRIPT start
+        echo "yes" | $REDISCLUSTER_SCRIPT create
+        cd -
+    else
+        cmd="numactl -m ${SERVER_SOCKET} -N ${SERVER_SOCKET} $REDIS_PATH/src/redis-server $REDIS_PATH/redis.conf --PORT ${PORT} --logfile $REDIS_PATH/server.log --loadmodule $REDISEARCH_LIB --save \"\""
+        echo -e $cmd
+        $cmd &
     fi
 fi
-
-#---------------------------------------------- Run Redis Instance with Redisearch module ----------------------------------------------------------
-
-echo "Killing existing redis server instances and remove rdb files..."
-killall -9 redis-server
-
-if [ "$REDIS_CLUSTER" -eq 1 ]; then
-    cd $REDIS_PATH/utils/create-cluster
-    REDISCLUSTER_SCRIPT=$REDIS_PATH/utils/create-cluster/create-cluster
-    $REDISCLUSTER_SCRIPT stop
-    $REDISCLUSTER_SCRIPT clean
-    cd -
-else
-    rm -f ${REDIS_PATH}/*.rdb
-fi
-
-sleep 10
-
-if [ "$REDIS_CLUSTER" -eq 1 ]; then
-    cd $REDIS_PATH/utils/create-cluster
-    REDISCLUSTER_CONFIG=$REDIS_PATH/utils/create-cluster/config.sh
-    echo "PORT=$((PORT-1))" > $REDISCLUSTER_CONFIG
-    echo "NODES=$CLUSTER_NODES" >> $REDISCLUSTER_CONFIG
-    echo "REPLICAS=$CLUSTER_REPLICAS" >> $REDISCLUSTER_CONFIG
-    echo "ADDITIONAL_OPTIONS='--loadmodule $REDISEARCH_LIB'" >> $REDISCLUSTER_CONFIG
-    $REDISCLUSTER_SCRIPT start
-    echo "yes" | $REDISCLUSTER_SCRIPT create
-    cd -
-else
-    cmd="numactl -m ${SERVER_SOCKET} -N ${SERVER_SOCKET} $REDIS_PATH/src/redis-server $REDIS_PATH/redis.conf --PORT ${PORT} --logfile $REDIS_PATH/server.log --loadmodule $REDISEARCH_LIB --save \"\""
-    echo -e $cmd
-    $cmd &
-fi
-
 
 Redis_Ping=$(${REDIS_PATH}/src/redis-cli -h localhost -p ${PORT} ping )
 echo "Waiting for redis server to be ready..."
@@ -141,4 +144,4 @@ done
 
 #-------------------------------------------------------- Run Vector-db-benchmark ------------------------------------------------------------------
 
-REDIS_CLUSTER=$REDIS_CLUSTER REDIS_PORT=$PORT $PYTHON_PATH $VECTORDB_BENCHMARK_PATH/run.py --engines redis-m-$M-ef-$EF_CONSTRUCTION --datasets ${DATASET_DICT[$DATASET_SIZE]} --host localhost --no-skip-if-exists
+REDIS_CLUSTER=$REDIS_CLUSTER REDIS_PORT=$PORT $PYTHON_PATH $VECTORDB_BENCHMARK_PATH/run.py --engines redis-m-$M-ef-$EF_CONSTRUCTION --datasets ${DATASET_DICT[$DATASET_SIZE]} --host localhost --no-skip-if-exists $ADDITIONAL_FLAGS
