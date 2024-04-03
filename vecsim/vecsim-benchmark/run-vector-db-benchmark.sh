4dd4e3fcc44f31217996d253476af8dc9311ce5e
Marcin Poźniak <marcin.pozniak@intel.com>
2024-04-03 11:19:30 +0200

    Add emon processing in remote scenario

--- vecsim/vecsim-benchmark/run-vector-db-benchmark.sh
+++ vecsim/vecsim-benchmark/run-vector-db-benchmark.sh
@@ -133,7 +133,7 @@ if [[ ${RUN_EMON} == true ]]; then
     fi
     if [[ ${SERVER_REMOTE} == true ]]; then
         $SSH_COMMAND "${EMON_FOLDER}/emon -stop"
-        cmd="${EMON_FOLDER}/emon -collect-edp -f redis-${DATASET_SIZE}-m-${M}-ef-${EF_CONSTRUCTION}-emon.dat"
+        cmd="${EMON_FOLDER}/emon -collect-edp -f ${HOME_PATH}/redis-${DATASET_SIZE}-m-${M}-ef-${EF_CONSTRUCTION}-emon.dat"
         nohup $SSH_COMMAND "$cmd &" &
     fi
 fi
@@ -148,5 +148,8 @@ if [[ ${RUN_EMON} == true ]]; then
     fi
     if [[ ${SERVER_REMOTE} == true ]]; then
         $SSH_COMMAND "${EMON_FOLDER}/emon -stop"
+        scp -i ${SSH_KEY_PATH}/${SSH_KEY_NAME} ../../shared-scripts/emon_process.sh ${LOGIN_ID}@${TARGET}:${HOME_PATH}/emon_proccess.sh
+        scp -i ${SSH_KEY_PATH}/${SSH_KEY_NAME} ${EMON_CONFIG_FILE} ${LOGIN_ID}@${TARGET}:${HOME_PATH}/pyedp_config.txt
+        $SSH_COMMAND "export EMON_CONFIG_FILE=$HOME_PATH/pyedp_config.txt && bash ${HOME_PATH}/emon_process.sh"
     fi
 fi
