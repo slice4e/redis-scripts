f0de6b12d7bad8f5ab5971b9dc58e71067d53088
Marcin Poźniak <marcin.pozniak@intel.com>
2025-01-27 10:05:19 +0100

    Create file for each top value

--- vecsim/SVS_with_fixed_ws_search.py
+++ vecsim/SVS_with_fixed_ws_search.py
@@ -7,6 +7,124 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.chart import BarChart, Reference
 
 
+fill_green = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+
+
+def highlight_cells(ws, top_n, reverse=False):
+    values = []
+    for row in ws.iter_rows(min_row=6, min_col=3):
+        for cell in row:
+            try:
+                cell_value = float(cell.value)
+                values.append((cell_value, cell))
+            except (TypeError, ValueError):
+                continue
+    values.sort(key=lambda x: x[0], reverse=reverse)
+    for i in range(min(top_n, len(values))):
+        values[i][1].fill = fill_green
+
+
+def create_excel_for_top(df_max_rps, top):
+    df_max_rps = df_max_rps[df_max_rps["top"] == top]
+    df_total_time = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'top', 'quantization', 'ws_construction'], values='total_time')
+    df_mean_time = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'top', 'quantization', 'ws_construction'], values='mean_time')
+    df_precision = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'top', 'quantization', 'ws_construction'], values='precision')
+    df_rps = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'top', 'quantization', 'ws_construction', ], values='rps')
+
+    df_uploading_time = pd.DataFrame.from_dict(settings, orient='index')[['uploading_time']]
+    df_used_memory = pd.DataFrame.from_dict(settings, orient='index')[['used_memory']]
+
+
+    quantization_values = df_max_rps['quantization'].unique()
+    parallel_values = df_max_rps['parallel'].unique()
+    closest_precision_rps = []
+
+    for quant in quantization_values:
+        for parallel in parallel_values:
+            df_quant_parallel = df_max_rps[(df_max_rps['quantization'] == quant) & (df_max_rps['parallel'] == parallel)]
+            if not df_quant_parallel.empty:
+                df_quant_parallel['precision_diff'] = abs(df_quant_parallel['precision'] - 0.95)
+                closest_row = df_quant_parallel.loc[df_quant_parallel['precision_diff'].idxmin()]
+                closest_precision_rps.append({
+                    'quantization': quant,
+                    'parallel': parallel,
+                    'precision': closest_row['precision'],
+                    'rps': closest_row['rps']
+                })
+
+    df_closest_precision_rps = pd.DataFrame(closest_precision_rps)
+    df_rps_pivot = df_closest_precision_rps.pivot(index='parallel', columns='quantization', values='rps')
+    df_precision_pivot = df_closest_precision_rps.pivot(index='parallel', columns='quantization', values='precision')
+
+    excel_path = f"summary/results-top{top}.xlsx"
+    with pd.ExcelWriter(excel_path) as writer:
+        df_total_time.to_excel(writer, sheet_name="Total Time")
+        df_mean_time.to_excel(writer, sheet_name="Mean Time")
+        df_precision.to_excel(writer, sheet_name="Precision")
+        df_rps.to_excel(writer, sheet_name="RPS")
+        df_uploading_time.to_excel(writer, sheet_name="Uploading Time")
+        df_used_memory.to_excel(writer, sheet_name="Used Memory")
+
+        worksheet = writer.book.create_sheet("Precision 95")
+        worksheet.title = "Precision 95"
+        
+        worksheet.cell(row=1, column=1, value="RPS")
+        for r in dataframe_to_rows(df_rps_pivot, index=True, header=True):
+            worksheet.append(r)
+        
+        start_row = df_rps_pivot.shape[0] + 5
+        worksheet.cell(row=start_row, column=1, value="Precision")
+        for r in dataframe_to_rows(df_precision_pivot, index=True, header=True):
+            worksheet.append(r)
+
+        # Add Bar chart to the sheet
+        chart = BarChart()
+        chart.title = "RPS @ ~95%"
+        chart.x_axis.title = "Parallel"
+        chart.y_axis.title = "RPS"
+
+        data = Reference(worksheet, min_col=2, min_row=2, max_row=df_rps_pivot.shape[0] + 3, max_col=df_rps_pivot.shape[1] + 1)
+        categories = Reference(worksheet, min_col=1, min_row=2, max_row=df_rps_pivot.shape[0] + 3)
+        chart.add_data(data, titles_from_data=True)
+        chart.set_categories(categories)
+        chart.shape = 4
+        worksheet.add_chart(chart, "E2")
+
+
+    wb = load_workbook(excel_path)
+
+
+    for sheet_name in ["Total Time", "Mean Time"]:
+        ws = wb[sheet_name]
+        highlight_cells(ws, top_n=3, reverse=False)
+        ws.column_dimensions['B'].width = 20
+
+    for sheet_name in ["Precision", "RPS"]:
+        ws = wb[sheet_name]
+        highlight_cells(ws, top_n=3, reverse=True)
+        ws.column_dimensions['B'].width = 20 
+
+    for sheet_name in ["Uploading Time", "Used Memory"]:
+        ws = wb[sheet_name]
+        ws.column_dimensions['A'].width = 40 
+        ws.column_dimensions['B'].width = 20
+        min_value = None
+        min_cell = None
+        for row in ws.iter_rows(min_row=2, min_col=2):
+            for cell in row:
+                try:
+                    cell_value = float(cell.value)
+                    if min_value is None or cell_value < min_value:
+                        min_value = cell_value
+                        min_cell = cell
+                except (TypeError, ValueError):
+                    continue
+        if min_cell:
+            min_cell.fill = fill_green
+
+    wb.save(excel_path)
+
+
 paths = []
 path = os.getcwd()
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
@@ -32,6 +150,7 @@ for file in files:
             result = {}
             experiment_name = data["params"]["experiment"]
             result["parallel"] = data["params"]["parallel"]
+            result["top"] = data["params"].get("top", 100)
             result["total_time"] = data["results"]["total_time"]
             result["mean_time"] = data["results"]["mean_time"]
             result["precision"] = data["results"]["mean_precisions"]
@@ -50,116 +169,8 @@ with open(result_json_path, "w") as f:
 
 df = pd.DataFrame(results)
 
-idx = df.groupby(['parallel', 'ws_search', 'graph_degree', 'num_threads', 'quantization', 'ws_construction'])['rps'].idxmax()
+idx = df.groupby(['parallel', 'ws_search', 'graph_degree', 'num_threads', 'top', 'quantization', 'ws_construction'])['rps'].idxmax()
 df_max_rps = df.loc[idx]
+create_excel_for_top(df_max_rps, 10)
+create_excel_for_top(df_max_rps, 100)
 
-df_total_time = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'quantization', 'ws_construction'], values='total_time')
-df_mean_time = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'quantization', 'ws_construction'], values='mean_time')
-df_precision = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'quantization', 'ws_construction'], values='precision')
-df_rps = df_max_rps.pivot(index=['parallel', 'ws_search'], columns=['graph_degree', 'num_threads', 'quantization', 'ws_construction', ], values='rps')
-
-df_uploading_time = pd.DataFrame.from_dict(settings, orient='index')[['uploading_time']]
-df_used_memory = pd.DataFrame.from_dict(settings, orient='index')[['used_memory']]
-
-
-quantization_values = df_max_rps['quantization'].unique()
-parallel_values = df_max_rps['parallel'].unique()
-closest_precision_rps = []
-
-for quant in quantization_values:
-    for parallel in parallel_values:
-        df_quant_parallel = df_max_rps[(df_max_rps['quantization'] == quant) & (df_max_rps['parallel'] == parallel)]
-        if not df_quant_parallel.empty:
-            df_quant_parallel['precision_diff'] = abs(df_quant_parallel['precision'] - 0.95)
-            closest_row = df_quant_parallel.loc[df_quant_parallel['precision_diff'].idxmin()]
-            closest_precision_rps.append({
-                'quantization': quant,
-                'parallel': parallel,
-                'precision': closest_row['precision'],
-                'rps': closest_row['rps']
-            })
-
-df_closest_precision_rps = pd.DataFrame(closest_precision_rps)
-df_rps_pivot = df_closest_precision_rps.pivot(index='parallel', columns='quantization', values='rps')
-df_precision_pivot = df_closest_precision_rps.pivot(index='parallel', columns='quantization', values='precision')
-
-excel_path = "summary/test-results.xlsx"
-with pd.ExcelWriter(excel_path) as writer:
-    df_total_time.to_excel(writer, sheet_name="Total Time")
-    df_mean_time.to_excel(writer, sheet_name="Mean Time")
-    df_precision.to_excel(writer, sheet_name="Precision")
-    df_rps.to_excel(writer, sheet_name="RPS")
-    df_uploading_time.to_excel(writer, sheet_name="Uploading Time")
-    df_used_memory.to_excel(writer, sheet_name="Used Memory")
-
-    worksheet = writer.book.create_sheet("Precision 95")
-    worksheet.title = "Precision 95"
-    
-    worksheet.cell(row=1, column=1, value="RPS")
-    for r in dataframe_to_rows(df_rps_pivot, index=True, header=True):
-        worksheet.append(r)
-    
-    start_row = df_rps_pivot.shape[0] + 5
-    worksheet.cell(row=start_row, column=1, value="Precision")
-    for r in dataframe_to_rows(df_precision_pivot, index=True, header=True):
-        worksheet.append(r)
-
-    # Add Bar chart to the sheet
-    chart = BarChart()
-    chart.title = "RPS @ ~95%"
-    chart.x_axis.title = "Parallel"
-    chart.y_axis.title = "RPS"
-
-    data = Reference(worksheet, min_col=2, min_row=2, max_row=df_rps_pivot.shape[0] + 3, max_col=df_rps_pivot.shape[1] + 1)
-    categories = Reference(worksheet, min_col=1, min_row=2, max_row=df_rps_pivot.shape[0] + 3)
-    chart.add_data(data, titles_from_data=True)
-    chart.set_categories(categories)
-    chart.shape = 4
-    worksheet.add_chart(chart, "E2")
-
-
-wb = load_workbook(excel_path)
-fill_green = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
-
-def highlight_cells(ws, top_n, reverse=False):
-    values = []
-    for row in ws.iter_rows(min_row=6, min_col=3):
-        for cell in row:
-            try:
-                cell_value = float(cell.value)
-                values.append((cell_value, cell))
-            except (TypeError, ValueError):
-                continue
-    values.sort(key=lambda x: x[0], reverse=reverse)
-    for i in range(min(top_n, len(values))):
-        values[i][1].fill = fill_green
-
-for sheet_name in ["Total Time", "Mean Time"]:
-    ws = wb[sheet_name]
-    highlight_cells(ws, top_n=3, reverse=False)
-    ws.column_dimensions['B'].width = 20
-
-for sheet_name in ["Precision", "RPS"]:
-    ws = wb[sheet_name]
-    highlight_cells(ws, top_n=3, reverse=True)
-    ws.column_dimensions['B'].width = 20 
-
-for sheet_name in ["Uploading Time", "Used Memory"]:
-    ws = wb[sheet_name]
-    ws.column_dimensions['A'].width = 40 
-    ws.column_dimensions['B'].width = 20
-    min_value = None
-    min_cell = None
-    for row in ws.iter_rows(min_row=2, min_col=2):
-        for cell in row:
-            try:
-                cell_value = float(cell.value)
-                if min_value is None or cell_value < min_value:
-                    min_value = cell_value
-                    min_cell = cell
-            except (TypeError, ValueError):
-                continue
-    if min_cell:
-        min_cell.fill = fill_green
-
-wb.save(excel_path)
--- vecsim/excel_hnsw.py
+++ vecsim/excel_hnsw.py
@@ -4,6 +4,80 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
+
+fill_green = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+
+
+def highlight_cells(ws, top_n, reverse=False):
+    values = []
+    for row in ws.iter_rows(min_row=6, min_col=3):
+        for cell in row:
+            try:
+                cell_value = float(cell.value)
+                values.append((cell_value, cell))
+            except (TypeError, ValueError):
+                continue
+    values.sort(key=lambda x: x[0], reverse=reverse)
+    for i in range(min(top_n, len(values))):
+        values[i][1].fill = fill_green
+
+
+
+def create_excel_for_top(df_max_rps, top):
+    df_max_rps = df_max_rps[df_max_rps["top"] == top]
+    # Create dataframe for each table
+    df_total_time = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['top', 'M',  'ef_construction'], values='total_time')
+    df_mean_time = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['top', 'M', 'ef_construction'], values='mean_time')
+    df_precision = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['top', 'M', 'ef_construction'], values='precision')
+    df_rps = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['top', 'M', 'ef_construction', ], values='rps')
+
+    # Create DataFrame for used_mem and uploading_Time
+    df_uploading_time = pd.DataFrame.from_dict(settings, orient='index')[['uploading_time']]
+    df_used_memory = pd.DataFrame.from_dict(settings, orient='index')[['used_memory']]
+
+    # Save to excel file
+    excel_path = f"summary/results-hnsw-top{top}.xlsx"
+    with pd.ExcelWriter(excel_path) as writer:
+        df_total_time.to_excel(writer, sheet_name="Total Time")
+        df_mean_time.to_excel(writer, sheet_name="Mean Time")
+        df_precision.to_excel(writer, sheet_name="Precision")
+        df_rps.to_excel(writer, sheet_name="RPS")
+        df_uploading_time.to_excel(writer, sheet_name="Uploading Time")
+        df_used_memory.to_excel(writer, sheet_name="Used Memory")
+
+    wb = load_workbook(excel_path)
+
+    for sheet_name in ["Total Time", "Mean Time"]:
+        ws = wb[sheet_name]
+        highlight_cells(ws, top_n=3, reverse=False)
+        ws.column_dimensions['B'].width = 20
+
+    for sheet_name in ["Precision", "RPS"]:
+        ws = wb[sheet_name]
+        highlight_cells(ws, top_n=3, reverse=True)
+        ws.column_dimensions['B'].width = 20
+
+    # Highlight lowest value in uploading time and used mem
+    for sheet_name in ["Uploading Time", "Used Memory"]:
+        ws = wb[sheet_name]
+        ws.column_dimensions['A'].width = 40
+        ws.column_dimensions['B'].width = 20
+        min_value = None
+        min_cell = None
+        for row in ws.iter_rows(min_row=2, min_col=2):
+            for cell in row:
+                try:
+                    cell_value = float(cell.value)
+                    if min_value is None or cell_value < min_value:
+                        min_value = cell_value
+                        min_cell = cell
+                except (TypeError, ValueError):
+                    continue
+        if min_cell:
+            min_cell.fill = fill_green
+
+    wb.save(excel_path)
+
 paths = []
 path = os.getcwd()
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
@@ -28,6 +102,7 @@ for file in files:
             experiment_name = data["params"]["experiment"]
             result["parallel"] = data["params"]["parallel"]
             result["ef_search"] = data["params"]["search_params"]["ef"]
+            result["top"] = data["params"].get("top", 100)
             result["total_time"] = data["results"]["total_time"]
             result["mean_time"] = data["results"]["mean_time"]
             result["precision"] = data["results"]["mean_precisions"]
@@ -41,76 +116,11 @@ with open(result_json_path, "w") as f:
     json.dump(results, f)
 
 
-# Tworzenie DataFrame
 df = pd.DataFrame(results)
 
-# Wybieranie wyników z największym rps dla każdej konfiguracji
-idx = df.groupby(['parallel', 'ef_search', 'M', 'ef_construction'])['rps'].idxmax()
+# Take the highest rps from each table
+idx = df.groupby(['parallel', 'ef_search', 'top', 'M', 'ef_construction'])['rps'].idxmax()
 df_max_rps = df.loc[idx]
 
-# Tworzenie DataFrame'ów dla każdej tabeli
-df_total_time = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['M',  'ef_construction'], values='total_time')
-df_mean_time = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['M', 'ef_construction'], values='mean_time')
-df_precision = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['M', 'ef_construction'], values='precision')
-df_rps = df_max_rps.pivot(index=['parallel', 'ef_search'], columns=['M', 'ef_construction', ], values='rps')
-
-# Tworzenie DataFrame dla uploading_time i used_memory
-df_uploading_time = pd.DataFrame.from_dict(settings, orient='index')[['uploading_time']]
-df_used_memory = pd.DataFrame.from_dict(settings, orient='index')[['used_memory']]
-
-# Zapisywanie do pliku Excel
-excel_path = "summary/results.xlsx"
-with pd.ExcelWriter(excel_path) as writer:
-    df_total_time.to_excel(writer, sheet_name="Total Time")
-    df_mean_time.to_excel(writer, sheet_name="Mean Time")
-    df_precision.to_excel(writer, sheet_name="Precision")
-    df_rps.to_excel(writer, sheet_name="RPS")
-    df_uploading_time.to_excel(writer, sheet_name="Uploading Time")
-    df_used_memory.to_excel(writer, sheet_name="Used Memory")
-
-wb = load_workbook(excel_path)
-fill_green = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
-
-def highlight_cells(ws, top_n, reverse=False):
-    values = []
-    for row in ws.iter_rows(min_row=6, min_col=3):
-        for cell in row:
-            try:
-                cell_value = float(cell.value)
-                values.append((cell_value, cell))
-            except (TypeError, ValueError):
-                continue
-    values.sort(key=lambda x: x[0], reverse=reverse)
-    for i in range(min(top_n, len(values))):
-        values[i][1].fill = fill_green
-
-for sheet_name in ["Total Time", "Mean Time"]:
-    ws = wb[sheet_name]
-    highlight_cells(ws, top_n=3, reverse=False)
-    ws.column_dimensions['B'].width = 20
-
-for sheet_name in ["Precision", "RPS"]:
-    ws = wb[sheet_name]
-    highlight_cells(ws, top_n=3, reverse=True)
-    ws.column_dimensions['B'].width = 20  # Zmiana szerokości kolumny B
-
-# Zmiana szerokości kolumny A i B oraz zaznaczenie najmniejszej wartości w arkuszach Uploading Time i Used Memory
-for sheet_name in ["Uploading Time", "Used Memory"]:
-    ws = wb[sheet_name]
-    ws.column_dimensions['A'].width = 40  # Zmiana szerokości kolumny A
-    ws.column_dimensions['B'].width = 20  # Zmiana szerokości kolumny B
-    min_value = None
-    min_cell = None
-    for row in ws.iter_rows(min_row=2, min_col=2):
-        for cell in row:
-            try:
-                cell_value = float(cell.value)
-                if min_value is None or cell_value < min_value:
-                    min_value = cell_value
-                    min_cell = cell
-            except (TypeError, ValueError):
-                continue
-    if min_cell:
-        min_cell.fill = fill_green
-
-wb.save(excel_path)
+create_excel_for_top(df_max_rps, 10)
+create_excel_for_top(df_max_rps, 100)
